92bc565e3f4d38e8c1896588b1b921a63759b051
Atharv Maan <atharv24@live.in>
2019-07-31 00:14:26 +0530

    infer num_batches from dataset

--- DCGAN/data_loader.py
+++ DCGAN/data_loader.py
@@ -10,12 +10,16 @@ def load_data_mnist(batch_size):
     Will be divided into (60000/batch_size) batches of (batch_size) each.
     '''
     (images, _), (_, _) = mnist.load_data()
+
+    size = images.shape[0]
+    num_batches = size//batch_size
+
     images = images.reshape(images.shape[0], 28, 28, 1).astype('float32')
     images = (images - 127.5)/127.5
 
     dataset = Dataset.from_tensor_slices(images).shuffle(buffer_size_mnist).batch(batch_size, drop_remainder = True)
     
-    return dataset
+    return dataset, num_batches
 
 def load_data_cifar(batch_size):
     '''
@@ -23,10 +27,14 @@ def load_data_cifar(batch_size):
     Will be divided into (50000/batch_size) batches of (batch_size) each.
     '''
     (images, _), (_, _) = cifar10.load_data()
+
+    size = images.shape[0]
+    num_batches = size//batch_size
+
     images = images.reshape(images.shape[0], 32, 32, 3).astype('float32')
     images = (images - 127.5)/127.5
 
     dataset = Dataset.from_tensor_slices(images).shuffle(buffer_size_cifar).batch(batch_size, drop_remainder = True)
     
-    return dataset
+    return dataset, num_batches
     
